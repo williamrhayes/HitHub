af090eb5e2e19dbacd34e15928982742a9dae672
madmaxvoltron <madmaxvoltron@gmail.com>
2023-10-27 22:36:26 +0200

    auto change if 0 bet history

--- website/home.js
+++ website/home.js
@@ -1,5 +1,9 @@
-const history1 = document.getElementById(his1)
-const history2 = document.getElementById(his2)
-const history3 = document.getElementById(his3)
-const history4 = document.getElementById(his4)
-const history5 = document.getElementById(his5)
+const history1 = document.getElementById("his1")
+const history2 = document.getElementById("his2")
+const history3 = document.getElementById("his3")
+const history4 = document.getElementById("his4")
+const history5 = document.getElementById("his5")
+
+if (history1 === '') {
+    history1.textContent = 'ⓘ no history found'
+}
