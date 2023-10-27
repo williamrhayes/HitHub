554f385a0c4c1263f42c411816a760ef0a7c45c9
madmaxvoltron <madmaxvoltron@gmail.com>
2023-10-27 13:02:06 +0200

    start historry changer

---
+++ website/home.js
@@ -0,0 +1,5 @@
+const history1 = document.getElementById(his1)
+const history2 = document.getElementById(his2)
+const history3 = document.getElementById(his3)
+const history4 = document.getElementById(his4)
+const history5 = document.getElementById(his5)
