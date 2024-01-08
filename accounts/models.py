7af949be0c31c74c714138d9c40cd29133e95fa4
williamrhayes <williamhayes323@gmail.com>
2024-01-07 17:46:51 -0800

    changed currency from coin to hitcoin

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    coin = models.DecimalField(max_digits=100, decimal_places=2, default=1_000.00)
+    hitcoin = models.DecimalField(max_digits=100, decimal_places=2, default=1_000.00)
